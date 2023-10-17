22483f1f4961bdd2000587007861ec8786801bde
AFQcyan <91124299+AFQcyan@users.noreply.github.com>
2023-10-17 15:21:50 +0900

    contact Nav 완성

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -9,7 +9,41 @@ const Nav = (props) => {
     <Fragment>
       <Container>
         <ul id="Nav">
-          <li className="each-nav">ABOUT ME</li>
+          <li className="each-nav">
+            <ul>
+              <li>MAIN</li>
+              <div className="nav-seperate"></div>
+              <li>MAIN</li>
+            </ul>
+          </li>
+          <li className="each-nav">
+            <ul>
+              <li>ABOUT ME</li>
+              <div className="nav-seperate"></div>
+              <li>ABOUT ME</li>
+            </ul>
+          </li>
+          <li className="each-nav">
+            <ul>
+              <li>SERVICES</li>
+              <div className="nav-seperate"></div>
+              <li>SERVICES</li>
+            </ul>
+          </li>
+          <li className="each-nav">
+            <ul>
+              <li>PORTFOLIO</li>
+              <div className="nav-seperate"></div>
+              <li>PORTFOLIO</li>
+            </ul>
+          </li>
+          <li className="each-nav">
+            <ul>
+              <li>CONTACT</li>
+              <div className="nav-seperate"></div>
+              <li>CONTACT</li>
+            </ul>
+          </li>
         </ul>
       </Container>
     </Fragment>
