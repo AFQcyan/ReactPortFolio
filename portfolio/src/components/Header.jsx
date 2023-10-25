1709d97fd3e2c00b4af9e5d237b606777bce48c1
AFQcyan <91124299+AFQcyan@users.noreply.github.com>
2023-10-25 22:48:12 +0900

    한건 없지만, 블로그 완성

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -3,14 +3,17 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 // components
 import Nav from "./Nav";
+import Main from "./Index";
 const Header = (props) => {
   const { state, stateHandler } = props;
 
   return (
     <Fragment>
-      {/* <Container className="Main mainContainer">
-        <Col xs={12}></Col>
-      </Container> */}
+      <Container className="Main mainContainer">
+        <Col xs={12}>
+          <Main state={state} stateHandler={stateHandler}></Main>
+        </Col>
+      </Container>
       <Container className="Nav mainContainer">
         <Col xs={12} id="logoImg"></Col>
         <Col xs={{ span: 8, offset: 2 }}>
---
+++ portfolio/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import { React, Fragment } from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+const Main = (props) => {
+  const { state, stateHandler } = props;
+
+  return (
+    <Fragment>
+      <div id="indexContainer"></div>
+    </Fragment>
+  );
+};
+
+export default Main;
