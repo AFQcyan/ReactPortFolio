d1bf6e1a0455fe2a52f82182dce26e7a563c6729
AFQcyan <91124299+AFQcyan@users.noreply.github.com>
2024-01-17 12:57:27 +0900

    SB like J SB

--- portfolio/src/components/SkillCount.jsx
+++ portfolio/src/components/SkillCount.jsx
@@ -1,6 +1,7 @@
 import { React, Fragment, useState, useEffect, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import ParallaxObject from "./ParallaxObject";
 import SkillDetail from "./SkillDetail";
@@ -8,9 +9,15 @@ import SkillDetail from "./SkillDetail";
 const SkillCount = (props) => {
   const { skillIdx, setSkillIdx } = props;
 
+  const stateChangeBtn = useRef(null);
+
   const clickButton = (e) => {
-    setSkillIdx(e.target.dataset.idx);
+    console.log(e.target);
+    setSkillIdx(parseInt(e.target.dataset.idx));
   };
+
+  // stateChangeBtn.addEventListener()
+  
 
   const ruleCalc = (targetNum) => {
     if (targetNum >= 0) {
@@ -20,36 +27,96 @@ const SkillCount = (props) => {
     }
   };
 
+  
+
   useEffect(() => {
-    console.log("이 년은 돌아야 되는디?");
     Array.from(document.querySelectorAll("#skill-count input")).filter(
       (x) => x.dataset.idx === "0"
     )[0].checked = true;
   }, []);
-
+  
   useEffect(() => {
-    console.log(skillIdx);
+    console.log(`effect : ${skillIdx}`);
     const currSlide = Array.from(
       document.querySelectorAll("#skill-count input")
-    ).filter((x) => x.dataset.idx === ruleCalc(skillIdx).toString());
+    ).filter((x) => parseInt(x.dataset.idx) === ruleCalc(skillIdx));
     currSlide[0].checked = true;
   }, [skillIdx]);
 
   return (
     <Fragment>
-      <div id="skill-count" onChange={clickButton}>
-        <input type="radio" id="slide1" name="slide" data-idx="0" />
-        <label htmlFor="slide1"></label>
-        <input type="radio" id="slide2" name="slide" data-idx="1" />
-        <label htmlFor="slide2"></label>
-        <input type="radio" id="slide3" name="slide" data-idx="2" />
-        <label htmlFor="slide3"></label>
-        <input type="radio" id="slide4" name="slide" data-idx="3" />
-        <label htmlFor="slide4"></label>
-        <input type="radio" id="slide5" name="slide" data-idx="4" />
-        <label htmlFor="slide5"></label>
-        <input type="radio" id="slide6" name="slide" data-idx="5" />
-        <label htmlFor="slide6"></label>
+      <div id="skill-count" >
+        <input type="radio" id="slide1" name="slide" data-idx="0" onClick={clickButton}/>
+        <div>
+        <label htmlFor="slide1">
+          <div className="slide-text">
+            <p>Front-end</p>
+          </div>
+          <div className="slide-icon">
+          <FontAwesomeIcon icon="fa-solid fa-caret-right" style={{color: "#FFD43B"}} />
+          </div>
+        </label>
+        </div>
+        <input type="radio" id="slide2" name="slide" data-idx="1" onClick={clickButton}/>
+        <div>
+
+        <label htmlFor="slide2">
+          <div className="slide-text">
+            <p>Back-end</p>
+          </div>
+          <div className="slide-icon">
+          <FontAwesomeIcon icon="fa-solid fa-caret-right" style={{color: "#FFD43B"}} />
+          </div>
+        </label>
+        </div>
+        <input type="radio" id="slide3" name="slide" data-idx="2" onClick={clickButton}/>
+        <div>
+
+        <label htmlFor="slide3">
+          <div className="slide-text">
+            <p>Server</p>
+          </div>
+          <div className="slide-icon">
+          <FontAwesomeIcon icon="fa-solid fa-caret-right" style={{color: "#FFD43B"}} />
+          </div>
+        </label>
+        </div>
+        <input type="radio" id="slide4" name="slide" data-idx="3" onClick={clickButton}/>
+        <div>
+
+        <label htmlFor="slide4">
+          <div className="slide-text">
+            <p>IDE</p>
+          </div>
+          <div className="slide-icon">
+          <FontAwesomeIcon icon="fa-solid fa-caret-right" style={{color: "#FFD43B"}} />
+          </div>
+        </label>
+        </div>
+        <input type="radio" id="slide5" name="slide" data-idx="4" onClick={clickButton}/>
+        <div>
+
+        <label htmlFor="slide5">
+          <div className="slide-text">
+            <p>Tool</p>
+          </div>
+          <div className="slide-icon">
+          <FontAwesomeIcon icon="fa-solid fa-caret-right" style={{color: "#FFD43B"}} />
+          </div>
+        </label>
+        </div>
+        <input type="radio" id="slide6" name="slide" data-idx="5" onClick={clickButton}/>
+        <div>
+
+        <label htmlFor="slide6">
+          <div className="slide-text">
+            <p>Cerf</p>
+          </div>
+          <div className="slide-icon">
+          <FontAwesomeIcon icon="fa-solid fa-caret-right" style={{color: "#FFD43B"}} />
+          </div>
+        </label>
+        </div>
       </div>
     </Fragment>
   );
